88584d9d0e6d005cea35e702b45ba68d9710f236
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-26 09:51:10 +0530

    [Ritesh]add : Calculate Length Of Line

---
+++ LineComparsionProblem/Lengths.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparsionProblem
+{
+    public class Lengths
+    {
+        int X1,Y1,X2,Y2;
+        public Lengths(int LenX1, int LenY1, int LenX2, int LenY2)
+        {
+            this.X1 = LenX1;
+            this.Y1 = LenY1;
+            this.X2 = LenX2;
+            this.Y2 = LenY2;   
+        }
+        public void LengthOfLine()
+        {
+            double length = Math.Sqrt(Math.Pow((X2 - X1 ), 2) + Math.Pow((Y2 - Y1 ), 2));
+            Console.WriteLine($"Length Of Line Is {length}");
+        }
+    }
+}
---
+++ LineComparsionProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparsionProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line Comparsion Problem");
+            Lengths obj = new Lengths(9, 7, 2, 3);
+            obj.LengthOfLine();
+            Console.ReadLine();
+        }
+    }
+}
