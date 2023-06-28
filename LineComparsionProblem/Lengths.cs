3c8680750d9d7861242bc32964ce12b7750ab603
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-28 07:46:22 +0530

    [Ritesh]add : LineCompare

--- LineComparsionProblem/Lengths.cs
+++ LineComparsionProblem/Lengths.cs
@@ -11,9 +11,9 @@ namespace LineComparsionProblem
         int X1,Y1,X2,Y2;
         public Lengths(int LenX1, int LenY1, int LenX2, int LenY2)
         {
-            this.X1 = LenX1;
-            this.Y1 = LenY1;
+            this.X1 = LenX1;           
             this.X2 = LenX2;
+            this.Y1 = LenY1;
             this.Y2 = LenY2;   
         }
         public double LengthOfLine()
--- LineComparsionProblem/Program.cs
+++ LineComparsionProblem/Program.cs
@@ -12,10 +12,10 @@ namespace LineComparsionProblem
         {
             Console.WriteLine("Line Comparsion Problem");
             // For Line 1 Object
-            Lengths obj = new Lengths(9, 7, 2, 3);
+            Lengths obj = new Lengths(1,4,2,8);
             double Line1 = obj.LengthOfLine();
             // For Line 2 Object
-            Lengths objec = new Lengths(5,4,2,8);
+            Lengths objec = new Lengths(5,9,9,8);
             double Line2 = objec.LengthOfLine();
             Console.WriteLine("Length Of Second Line = " + Line2);
             // Equal Method Given Boolen Condition
@@ -26,8 +26,18 @@ namespace LineComparsionProblem
             else
             {
                 Console.WriteLine("No Both Lines Are UnEqual");
+                // To Check Which Line is Greater By Compare To Method
+                if(Line1.CompareTo(Line2) > 0)
+                {
+                    Console.WriteLine("Line 1 Greater To Line 2");
+                }
+                else
+                {
+                    Console.WriteLine("Line 1 Is Less Than 2");
+                }
             }
-            Console.ReadLine();
+
+              Console.ReadLine();
         }
     }
 }
